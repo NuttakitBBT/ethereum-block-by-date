c3664ca729dd62502f3050730f9fe669cf71b7a7
NuttakitDW <nuttakit@roostoo.com>
2022-12-09 17:55:21 +0700

    update

--- getDate/getDate.go
+++ getDate/getDate.go
@@ -2,8 +2,25 @@ package getDate
 
 import (
 	"fmt"
+	"context"
+    "log"
+	"math/big"
+	"github.com/ethereum/go-ethereum/ethclient"
 )
 
-func SayHi() {
-    fmt.Println("Hello world")
+
+
+func getDate(date string) {
+	client, err := ethclient.Dial("https://mainnet.infura.io/v3/8e832100bb9d451887b920a0935dc120")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    header, err := client.HeaderByNumber(context.Background(), nil)
+    if err != nil {
+        log.Fatal(err)
+    }
+	curBlock := header.Number.Uint64()
+	blockNumber := big.NewInt(curBlock)
+	unixCB := curBlock.Time()
 }
--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
     "context"
     "fmt"
     "log"
+	"math/big"
     "github.com/ethereum/go-ethereum/ethclient"
+	//"github.com/NuttakitBBT/ethereum-block-by-date/getDate"
 )
 
 func main() {
@@ -13,10 +15,20 @@ func main() {
         log.Fatal(err)
     }
 
-    header, err := client.HeaderByNumber(context.Background(), nil)
+    //header, err := client.HeaderByNumber(context.Background(), nil)
     if err != nil {
         log.Fatal(err)
     }
-	getDate.SayHi()
-    fmt.Println(header.Number.String()) // 5671744
+    blockNumber := big.NewInt(5671743)
+	block, err := client.BlockByNumber(context.Background(), blockNumber)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(block.Number().Uint64())     // 5671744
+	fmt.Println(block.Time())       // 1527211625
+	fmt.Println(block.Difficulty().Uint64()) // 3217000136609065
+	fmt.Println(block.Hash().Hex())          // 0x9e8751ebb5069389b855bba72d94902cc385042661498a415979b7b6ee9ba4b9
+	fmt.Println(len(block.Transactions()))   // 144
+	  
 }
